ee6ca4994c9036b84d2ed301a99a9e32c65f99f0
yana-chyp <yana.chyp@lnu.edu.ua>
2024-11-22 02:42:01 +0200

    improv: correct the matrix calculations
    uses finite element matrix, jacobi coefficient correctly. the result saves the expected sparse symmetric structure

--- 2d/system_2d.py
+++ 2d/system_2d.py
@@ -11,10 +11,10 @@ def set_up_matrix(d1, d2, p, m, element_type='D2QU4N'):
     #assume nodes are equidistant
     h_x = d1/p
     h_y = d2/m
-    J_x = 2/h_x
-    J_y = 2/h_y
+    J = h_x*h_y/4
 
-    matrix = [[0 for j in range(m+1)] for i in range(p+1)]
+    n = (p+1)*(m+1)
+    matrix = [[0 for j in range(n)] for i in range(n)]
 
     em = fe2d.element_matrix()
     #ioe - index of element
@@ -22,12 +22,59 @@ def set_up_matrix(d1, d2, p, m, element_type='D2QU4N'):
         #noe - nodes of element
         noe = elements[ioe]
         # print(noe)
+
+        # im = noe[0]//(p+1)
+        # jm = noe[0] - im * (p+1)
         for i in range(len(noe)):
             for j in range(len(noe)):
-                im = noe[0]//(p+1)
-                jm = noe[0] - im * (p+1)
-                value = em[i][j][0]*J_x**2 + em[i][j][1]*J_y**2
-                print("i = ", i, " j = ", j, " value = ", value)
-                matrix[im][jm] += value
+                im = noe[i]
+                jm = noe[j]
+                value = em[i][j]*J
 
+                # print("(", i, ",  ", j, ") -> (", im, ", ", jm, "); value = ", f"{value:.4f}")
+                matrix[im][jm] += value
+            # print('[' + ', '.join([f"{el:.8f}" for el in matrix[i]]) + ']')
     return matrix
+
+
+
+
+def get_boundary_points(p, m):
+    bounds = []
+    for j in range(p+1):
+        bounds.append(j)
+    for i in range(1, m+1):
+        index = (i+1)*(p+1)-1
+        bounds.append(index)
+    for j in reversed(range(p)):
+        index = p*(m+1) - 1 + j
+        bounds.append(index)
+    for i in reversed(range(1, m)):
+        index = i*(p+1)
+        bounds.append(index)
+    return bounds
+
+
+
+
+#let ug_i = f(x) = y - function defining the boundary conditions
+#here i = 1,...,4 means 4 intervals on the plane intersecting the Omega area
+# def get_boundary_points(NL , ug_1, ug_2, ug_3, ug_4):
+    # gamma_1 = []
+    # gamma_2 = []
+    # gamma_3 = []
+    # gamma_4 = []
+    # for i in range(len(NL)):
+    #     if NL[i][1]==ug_1(NL[i][0]):
+    #         gamma_1.append(NL[i])
+    #     elif NL[i][1]==ug_2(NL[i][0]):
+    #         gamma_2.append(NL[i])
+    #     elif NL[i][1]==ug_3(NL[i][0]):
+    #         gamma_3.append(NL[i])
+    #     elif NL[i][1]==ug_4(NL[i][0]):
+    #         gamma_4.append(NL[i])
+
+    # return[gamma_1, gamma_2, gamma_3, gamma_4]
+
+
+
